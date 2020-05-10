8c4467993c7c85c0e7d27ca58432971bc2a7e779
ad-dhawan <ankit.ad.dhawan@gmail.com>
2020-05-10 22:46:18 +0530

    car controller

---
+++ VehicleController/Assets/Scripts/CameraRotate.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRotate : MonoBehaviour {
+
+	public float CameraMoveSpeed = 120.0f;
+	public GameObject CameraFollowObj;
+	Vector3 FollowPos;
+	public float clampAngle = 80.0f;
+	public float inputSensitivity=150.0f;
+	public GameObject cameraObj;
+	public GameObject playerObj;
+	public float camDistanceXToPlayer;
+	public float camDistanceYToPlayer;
+	public float camDistanceZToPlayer;
+	public float mouseX;
+	public float mouseY;
+	public float finalInputX;
+	public float finalInputZ;
+	public float smoothX;
+	public float smoothY;
+	private float rotY = 0.0f;
+	private float rotX = 0.0f;
+
+
+	// Use this for initialization
+	void Start () {
+
+		Vector3 rot = transform.localRotation.eulerAngles;
+		rotY = rot.y;
+		rotX = rot.x;
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//rotation of sticks
+		float inputX = Input.GetAxis("RightStickHorizontal");
+		float inputZ = Input.GetAxis("RightStickVertical");
+		mouseX = Input.GetAxis("Mouse X");
+		mouseY = Input.GetAxis("Mouse Y");
+		finalInputX = inputX + mouseX;
+		finalInputZ = inputZ + mouseY;
+
+		rotY += finalInputX * inputSensitivity + Time.deltaTime;
+		rotX += finalInputZ * inputSensitivity + Time.deltaTime;
+
+		rotX = Mathf.Clamp (rotX, -clampAngle,clampAngle);
+		Quaternion localRotation = Quaternion.Euler (rotX, rotY, 0.0f);
+		transform.rotation = localRotation;
+	
+	}
+
+	void LateUpdate (){
+		cameraUpdater();
+	}
+
+	void cameraUpdater() {
+		Transform target = CameraFollowObj.transform;
+
+		float step = CameraMoveSpeed * Time.deltaTime;
+		transform.position = Vector3.MoveTowards (transform.position, target.position, step);
+	}
+}
---
+++ VehicleController/Assets/Scripts/car_controller.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class car_controller : MonoBehaviour {
+
+	private float horizontalInput;
+	private float verticalInput;
+	private float steeringAngle;
+
+	public WheelCollider frontLeftWheel, frontRightWheel;
+	public WheelCollider rearLeftWheel, rearRightWheel;
+	public Transform frontLeftTransform, frontRightTransform;
+	public Transform rearLeftTransform, rearRightTransform;
+
+	public float maxSteerAngle = 35;
+	public float motorForce = 55;
+
+
+	public void GetInput()
+	{
+		horizontalInput = Input.GetAxis("Horizontal");
+		verticalInput = Input.GetAxis("Vertical");
+	}
+
+	public void Steer()
+	{
+		steeringAngle = maxSteerAngle * horizontalInput;
+		frontLeftWheel.steerAngle = steeringAngle;
+		frontRightWheel.steerAngle = steeringAngle;
+	}
+
+	public void Accelerate()
+	{
+		frontLeftWheel.motorTorque = verticalInput * motorForce;
+		frontRightWheel.motorTorque = verticalInput * motorForce;
+	}
+
+	public void UpdateWheelPoses()
+	{
+		UpdateWheelPose(frontLeftWheel, frontLeftTransform);
+		UpdateWheelPose(frontRightWheel, frontRightTransform);
+		UpdateWheelPose(rearLeftWheel, rearLeftTransform);
+		UpdateWheelPose(rearRightWheel, rearRightTransform);
+	}
+
+	public void UpdateWheelPose(WheelCollider collider, Transform transform)
+	{
+		Vector3 pos = transform.position;
+		Quaternion quat = transform.rotation;
+
+		collider.GetWorldPose(out pos, out quat);
+
+		transform.position = pos;
+		transform.rotation = quat;
+	}
+
+	public void FixedUpdate()
+	{
+		GetInput();
+		Steer();
+		Accelerate();
+		UpdateWheelPoses();
+	}
+
+
+
+}
